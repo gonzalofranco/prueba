287f5aec8e85908912663c639f42c037459986c0
gonzalofranco <32277421+gonzalofranco@users.noreply.github.com>
2017-10-11 21:02:11 -0300

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-
-using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    srand(time(0));
+    list<Heladera> lista_heladera;
+    list<cocina> lista_cocina;
+    list<lavarropa> lista_lavarropa;
+    queue <int> cola;
+    cargar_lista(lista_heladera,lista_cocina,lista_lavarropa,cola);
+    mostrar_lista(lista_heladera,lista_cocina,lista_lavarropa,cola);
     return 0;
 }
